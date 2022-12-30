820646520a3a89fd5a0f68f8a3e50ffd2e457ead
RomaTheSun <pryimak.roma@gmail.com>
2022-12-30 14:51:34 +0200

    Update ProposalsPage.js

--- lok/src/components/ProposalsPage/ProposalsPage.js
+++ lok/src/components/ProposalsPage/ProposalsPage.js
@@ -11,60 +11,60 @@ import softservelogo from "../../img/proposals/softserve.png"
 import ubisoftlogo from "../../img/proposals/ubisoft.png"
 
 
-class ProposalsPage extends Component{
+class ProposalsPage extends Component {
 
 
-  render(){
-    return(
+  render() {
+    return (
       <div className="container-fluid background ">
-          <div className='text-center proposals-title'>Proposals from partners</div>
-          <hr className='col-4 underline-proposals'></hr>
+        <div className='text-center proposals-title'>Proposals from partners</div>
+        <hr className='col-4 underline-proposals'></hr>
         <div className='row justify-content-center'>
 
 
           <div className="card proposal-card col-12 col-md-3 col-xxl-2">
-            <img src={Proposal1} className="card-img-top" alt="..."/>
+            <img src={Proposal1} className="card-img-top" alt="..." />
             <div className="card-body">
-            <img src={sigmalogo} className="mx-auto d-block p-logo" alt="..."/>
-            <p className="card-text">
+              <img src={sigmalogo} className="mx-auto d-block p-logo" alt="..." />
+              <p className="card-text">
                 Your idea turned into a live product adjusted to the market needs & generating revenue.
 
                 We use a lean product development approach shortening your product time-to-market.
-            </p>
-            <div className="row justify-content-center">
-              <a href="#" className="btn col-6 btn-primary proposal-btn">More</a>
-            </div>
-            </div>
-          </div>
-
-          <div className="card proposal-card col-12 col-md-3 col-xxl-2">
-            <img src={Proposal2} className="card-img-top" alt="..."/>
-            <div className="card-body">
-            <img src={softservelogo} className="mx-auto d-block p-logo" alt="..."/>
-            <p className="card-text">
-            Reinforce your product with cutting-edge technologies to unlock its potential.
-
-Our R&D experts regularly generate innovative concepts for customers’ business.
-            </p>
-            <div className="row justify-content-center">
-              <a href="#" className="btn col-6 btn-primary proposal-btn">More</a>
-            </div>
+              </p>
+              <div className="row justify-content-center">
+                <a href="#" className="btn col-6 btn-primary proposal-btn">More</a>
+              </div>
             </div>
           </div>
 
           <div className="card proposal-card col-12 col-md-3 col-xxl-2">
-            <img src={Proposal3} className="card-img-top" alt="..."/>
+            <img src={Proposal2} className="card-img-top" alt="..." />
             <div className="card-body">
-            <img src={ubisoftlogo} className="mx-auto d-block p-logo" alt="..."/>
-            <p className="card-text">
-               
-Involve niche professionals when needed within an established collaboration setup.
+              <img src={softservelogo} className="mx-auto d-block p-logo" alt="..." />
+              <p className="card-text">
+                Reinforce your product with cutting-edge technologies to unlock its potential.
 
-We complement our services with Product Strategy, UX, CTO, and InfoSec consulting.
-            </p>
-            <div className="row justify-content-center">
-              <a href="#" className="btn col-6 btn-primary proposal-btn">More</a>
+                Our R&D experts regularly generate innovative concepts for customers’ business.
+              </p>
+              <div className="row justify-content-center">
+                <a href="#" className="btn col-6 btn-primary proposal-btn">More</a>
+              </div>
             </div>
+          </div>
+
+          <div className="card proposal-card col-12 col-md-3 col-xxl-2">
+            <img src={Proposal3} className="card-img-top" alt="..." />
+            <div className="card-body">
+              <img src={ubisoftlogo} className="mx-auto d-block p-logo" alt="..." />
+              <p className="card-text">
+
+                Involve niche professionals when needed within an established collaboration setup.
+
+                We complement our services with Product Strategy, UX, CTO, and InfoSec consulting.
+              </p>
+              <div className="row justify-content-center">
+                <a href="#" className="btn col-6 btn-primary proposal-btn">More</a>
+              </div>
             </div>
           </div>
 
