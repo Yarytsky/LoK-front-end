d22e5f78e4295a6583de5d1baea4ca3a53e108de
Lida Pasternak <45924939+LidaPasternak@users.noreply.github.com>
2023-03-16 14:59:49 +0200

    Email confirm done

--- lok/src/components/emailConfirm/emailConfirm.js
+++ lok/src/components/emailConfirm/emailConfirm.js
@@ -3,20 +3,41 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './emailConfirm.css';
 import {Route, Link, Routes, useNavigate} from 'react-router-dom';
-import Header from '../../components/header/header';
-import Footer from "../../components/footer/footer";
-import logo from "../../img/Bobrlogo.png"
-import verifyEmailPic from '../../img/verifyEmailPic.svg'
-
+import logo from "../../img/Bobrlogo.png";
+import verifyEmailPic from '../../img/emailVerify.png';
+import lock from "../../img/lockActivationCode.png";
+import key from '../../img/keyActivationCode.png'
+import check from '../../img/confirmEmailCheck.png'
 
 class EmailConfirm extends React.Component {
 
- 
+    showActivationCodeBlock = (e) => {
+        let verifyEmailBlock = document.getElementsByClassName("verifyEmailBlock")[0];
+        let activationCodeBlock = document.getElementsByClassName("activationCodeBlock")[0];
+        verifyEmailBlock.style.display = 'none';
+        activationCodeBlock.style.display = "flex";
+    }
+
+    activateAccount = (e) => {
+        let confirmSuccessScreen = document.getElementsByClassName("confirmSuccessScreen")[0];
+        confirmSuccessScreen.style.display = "flex";
+    }
 
   render() {
     return (
       <div>
         <div className='emailConfirmBackground'>
+            <div className = "confirmSuccessScreen">
+                <div className='confirmSuccessBlock'>
+                    <img src = {check}></img>
+                    <p>
+                        Success!
+                    </p>
+                    <p>
+                        Your email has been confirmed.
+                    </p>
+                </div>
+            </div>
             <div className='emailConfirmContainer'>
                 <div className = "confirmLogo"><img src = {logo}></img></div>
                 <div className = "verifyEmailBlock">
@@ -33,8 +54,27 @@ class EmailConfirm extends React.Component {
                         <div className='verifyEmailText2'>
                             Please click the button below to confirm your email address
                         </div>
-                        <div className='verifyEmailButton'>
+                        <div className='verifyEmailButton' onClick = {this.showActivationCodeBlock}>
                             Confirm email
+                        </div>
+                    </div>
+                </div>
+                <div className = "activationCodeBlock">
+                    <div className='activationCodePic'>
+                        <img src = {key}></img>
+                        <img src = {lock}></img>
+                    </div>
+                    <div className='activationCodeMain'>
+                        <div className='activationCodeTitle'>
+                            activation code
+                        </div>
+                        <input className='activationCodeInput'>
+                        </input>
+                        <div className='activationCodeText'>
+                            Copy your activation code and click the button below to activate your account
+                        </div>
+                        <div className='activationCodeButton' onClick = {this.activateAccount}>
+                            Activate your account
                         </div>
                     </div>
                 </div>
